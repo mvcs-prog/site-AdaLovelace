77602edb5b72053bb9b1c4e5802b35776b6cc487
Maria <mariavieira.reserva@outlook.com>
2025-07-24 20:57:56 -0300

    Primeiro commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function validaFormulario(){
     if (document.getElementById("nome").value != "" && 
     document.getElementById("email").value !="" &&
     document.getElementById("Telefone").value !=""){}
-        alert(Prontinho! Você receberá as novidades por email)
+        alert(Prontinho! Você receberá as novidades por email.)
     }
     else{
         alert(Por favor, preencha os campos nome e email)
